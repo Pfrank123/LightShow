e5e3bf6ff9a39b19ceddf53cd32f0a2dee7d0a49
nchandran17 <chandran717n@gmail.com>
2022-10-11 20:26:59 -0400

    switched to use Nitin's device instead of Phil's, still hardcoded

--- colorpoll.py
+++ colorpoll.py
@@ -82,7 +82,7 @@ def main():
                 lastShownGreen = green
                 lastShownBlue = blue
                 currentColorStr = newColorStr
-                os.system("particle call lightshow show " + currentColorStr)
+                os.system("particle call Nitin_dev show " + currentColorStr)
     except HttpError as err:
         print(err)
 
